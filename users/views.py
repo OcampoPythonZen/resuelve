4b17d705b9019e8c0e42c142afd67ee8b7cbd334
oocampoTVP <eocampo1986@gmail.com>
2018-11-14 12:14:10 -0600

    Primer cuatrimestre de consulta anual de users

--- users/views.py
+++ users/views.py
@@ -1,7 +1,30 @@
+
 from django.shortcuts import render
-from django.http import HttpResponse, request
+import requests
 
-# Create your views here.
-def index(request):
-    return HttpResponse('<center><h1>USUARIOS</h1></center>')
+def home(request):
+    url = 'https://us-central1-prueba-resuelve.cloudfunctions.net/users/2017-01-01/2017-04-01'
+    response = requests.get(url)
+    data = response.json()
 
+
+    context = {
+        'nombre' : data[0]['nombre'],
+        'apellido' : data[0]['apellido'],
+        'segundo_nombre' : data[0]['segundo_nombre'],
+        'segundo_apellido' : data[0]['segundo_apellido'],
+        'uid' : data[0]['uid'],
+        'email' : data[0]['email'],
+        'active' : data[0]['active'],
+        'created_at' : data[0]['created_at']
+    }
+
+
+    # for d in data:
+    #     context = {}
+    #     for k,v in d.items():
+    #         context[k]=v
+            #print(k,':',v)
+
+    template_name = 'home.html'
+    return render(request, template_name, context)
